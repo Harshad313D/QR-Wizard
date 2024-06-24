19bbbf3dbce04a93d85bab5a88de59fae25f9355
Harshad313D <hdexamples@gmail.com>
2024-06-24 15:57:23 +0530

    3rd commit

--- index.js
+++ index.js
@@ -40,16 +40,30 @@ qrInputTextEl.addEventListener("keyup", () => {
   }
 });
 
-downloadBtnEl.addEventListener("click", () => {
-  const qrCodeUrl = qrImageEl.src;
 
-  const a = document.createElement('a');
-  a.href = qrCodeUrl;
-  a.download = 'qrcode.png'; 
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+
+downloadBtnEl.addEventListener("click", async () => {
+  const { jsPDF } = window.jspdf;
+
+  // Create a new jsPDF instance
+  const doc = new jsPDF();
+
+  // Load the QR code image
+  const qrCodeUrl = qrImageEl.src;
+  const img = new Image();
+  img.crossOrigin = 'Anonymous'; // This may be required to prevent CORS issues
+
+  img.onload = () => {
+    doc.text('Welcome to QR Wizard! ', 70, 10); 
+    doc.text('Created by Harshad313D (github) ', 60, 140); 
+    doc.text('Scan the QR code below to access the content:', 50, 50); 
+    doc.addImage(img, 'PNG', 80, 60, 50, 50);
+    doc.save('qrcode.pdf');
+  };
+
+  img.src = qrCodeUrl;
 });
+
 
 document.addEventListener('contextmenu', function (event) {
   // Prevent right-click context menu
